09e1624c4331271d8b37aa88b04559b5edb61d1b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-17 11:14:51 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -22,7 +22,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        Benvolio => {
+          :name => ""
+          :age => ""
+          :attitude => ""
+        },
+        Mercutio => {
+          :name => ""
+          :age => ""
+          :attitude => ""
+        }
       ]
    }
 }
